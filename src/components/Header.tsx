6e796f0b738b18630ced24b826986c51219e2160
Akshay R (A-K-SHA-Y) <akkitricks@gmail.com>
2026-03-13 00:38:48 +0530

    Up

--- src/components/Header.tsx
+++ src/components/Header.tsx
@@ -7,38 +7,43 @@ const Header = () => {
   const [isOpen, setIsOpen] = useState(false);
   return (
     <header>
-      <div className="menu-main">
-        <div className="menu-btn">
-          <button onClick={() => setIsOpen((prev) => !prev)}>
-            <Icon.ListIcon size={32} />
-          </button>
+      <div className="header-wrap">
+        <div className="main-header">
+          <div className="menu-main">
+            <div className="menu-btn">
+              <button onClick={() => setIsOpen((prev) => !prev)}>
+                <Icon.ListIcon size={32} />
+              </button>
+            </div>
+            <div>
+              <NavLink to="/">Home</NavLink>
+            </div>
+          </div>
+          <nav>
+            <ul className="menu-options">
+              <li>
+                <NavLink to="/products">Shop</NavLink>
+              </li>
+              <li>
+                <NavLink to="/products?type=new-arrivals">New Arrivals</NavLink>
+              </li>
+              <li>
+                <NavLink to="/products?type=new-arrivals">Brands</NavLink>
+              </li>
+              <li></li>
+            </ul>
+          </nav>
+          <div className="user-wrap">
+            <div>
+              <Icon.ShoppingCartSimpleIcon size={32} />
+            </div>
+            <div>
+              <Icon.UserIcon size={32} />
+            </div>
+          </div>
         </div>
-        <div>
-          <NavLink to="/">Home</NavLink>
-        </div>
-      </div>
-      <nav>
-        <ul className={isOpen ? "menu-options" : "hide"}>
-          <li>
-            <NavLink to="/products">Shop</NavLink>
-          </li>
-          <li>
-            <NavLink to="/products?type=new-arrivals">New Arrivals</NavLink>
-          </li>
-          <li>
-            <NavLink to="/products?type=new-arrivals">Brands</NavLink>
-          </li>
-          <li>
-            <input type="text" placeholder="Search for Products..." />
-          </li>
-        </ul>
-      </nav>
-      <div className="user-wrap">
-        <div>
-          <Icon.ShoppingCartSimpleIcon size={32} />
-        </div>
-        <div>
-          <Icon.UserIcon size={32} />
+        <div className="search-wrap">
+          <input type="text" placeholder="Search for Products..." />
         </div>
       </div>
     </header>
