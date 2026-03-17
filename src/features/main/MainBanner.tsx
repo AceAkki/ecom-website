a7158678b0257d85521db1e137b372483dc6eabb
Akshay R (A-K-SHA-Y) <akkitricks@gmail.com>
2026-03-18 00:33:57 +0530

    Txt add

--- src/features/main/MainBanner.tsx
+++ src/features/main/MainBanner.tsx
@@ -1,9 +1,16 @@
 import BGImage from "../../assets/banner.png";
+import "./main.css";
+
 const MainBanner = () => {
   return (
     <div className="main-banner-wrap">
       <div className="txt-content">
         <h1>FIND CLOTHES THAT MATCHES YOUR STYLE</h1>
+        <p>
+          Browse through our diverse range of meticulously crafted garments,
+          designed to bring out your individuality and cater to your sense of
+          style.
+        </p>
       </div>
       <div className="image-wrap">
         <img src={BGImage} alt="" />
