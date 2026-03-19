3ef7a40fa7ab8e0b2ed5fb4a899c34cefa2d9f9f
Akshay R (A-K-SHA-Y) <akkitricks@gmail.com>
2026-03-19 20:44:16 +0530

    Style up

--- src/components/Header.tsx
+++ src/components/Header.tsx
@@ -7,52 +7,73 @@ const Header = () => {
   const [isOpen, setIsOpen] = useState(false);
   return (
     <header>
-      <div className="header-wrap">
-        <div className="main-header">
-          <div className="menu-main">
-            <div className="menu-btn">
-              <button
-                onClick={() => {
-                  setIsOpen((prev) => !prev);
-                  setTimeout(() => {
-                    setIsOpen(false);
-                  }, 4000);
-                }}
-              >
-                <Icon.ListIcon size={32} />
-              </button>
+      <div className="header-main-wrapper">
+        <div className="header-wrap">
+          <div className="main-header">
+            <div className="menu-main">
+              <div className="menu-btn">
+                <button
+                  onClick={() => {
+                    setIsOpen((prev) => !prev);
+                    setTimeout(() => {
+                      setIsOpen(false);
+                    }, 4000);
+                  }}
+                >
+                  <Icon.ListIcon size={32} />
+                </button>
+              </div>
+              <div>
+                <NavLink to="/">Home</NavLink>
+              </div>
             </div>
-            <div>
-              <NavLink to="/">Home</NavLink>
+            <nav className={isOpen ? "" : "mobile-hide"}>
+              <ul className="menu-options">
+                <li>
+                  <NavLink to="/products">All</NavLink>
+                </li>
+                <li>
+                  <NavLink to="/products?type=new-arrivals">
+                    Personal Care
+                  </NavLink>
+                </li>
+                <li>
+                  <NavLink to="/products?type=new-arrivals">
+                    Men's Fashion
+                  </NavLink>
+                </li>
+                <li>
+                  <NavLink to="/products?type=new-arrivals">
+                    Women's Fashion
+                  </NavLink>
+                </li>
+                <li>
+                  <NavLink to="/products?type=new-arrivals">
+                    Electronics
+                  </NavLink>
+                </li>
+                <li>
+                  <NavLink to="/products?type=new-arrivals">
+                    Home & Living
+                  </NavLink>
+                </li>
+                <li>
+                  <NavLink to="/products?type=new-arrivals">Vehicles</NavLink>
+                </li>
+              </ul>
+            </nav>
+            <div className="user-wrap">
+              <div>
+                <Icon.ShoppingCartSimpleIcon size={32} />
+              </div>
+              <div>
+                <Icon.UserIcon size={32} />
+              </div>
             </div>
           </div>
-          <nav className={isOpen ? "" : "mobile-hide"}>
-            <ul className="menu-options">
-              <li>
-                <NavLink to="/products">Shop</NavLink>
-              </li>
-              <li>
-                <NavLink to="/products?type=new-arrivals">New Arrivals</NavLink>
-              </li>
-              <li>
-                <NavLink to="/products?type=new-arrivals">Brands</NavLink>
-              </li>
-              <li>
-                <input type="text" placeholder="Search for Products..." />
-              </li>
-            </ul>
-          </nav>
-          <div className="user-wrap">
-            <div>
-              <Icon.ShoppingCartSimpleIcon size={32} />
-            </div>
-            <div>
-              <Icon.UserIcon size={32} />
-            </div>
+          <div className="search-wrap">
+            <input type="text" placeholder="Search for Products..." />
           </div>
-        </div>
-        <div className="search-wrap">
-          <input type="text" placeholder="Search for Products..." />
         </div>
       </div>
     </header>
