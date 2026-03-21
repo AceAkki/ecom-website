917c776da42f297a0c7f8a2e1661039e8754f8cc
Akshay R (A-K-SHA-Y) <akkitricks@gmail.com>
2026-03-21 23:59:14 +0530

    up

--- src/features/main/components/CategorySection.tsx
+++ src/features/main/components/CategorySection.tsx
@@ -2,6 +2,19 @@ const CategorySection = () => {
   return (
     <section>
       <h1>Category Section</h1>
+
+      <div className="category-wrap">
+        <div className="category-mini-wrapper">
+          <div className="category-item landscape">Personal</div>
+          <div className="category-item portrait">Fashion</div>
+          <div className="category-item portrait">Fashion</div>
+        </div>
+        <div className="category-mini-wrapper">
+          <div className="category-item portrait">Electronics</div>
+          <div className="category-item portrait">Living</div>
+          <div className="category-item landscape">Vehicles</div>
+        </div>
+      </div>
     </section>
   );
 };
