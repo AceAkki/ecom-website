c8f8af9a5b8ddafeebdc265e3002f21e9deb1bf9
Akshay R (A-K-SHA-Y) <akkitricks@gmail.com>
2026-03-22 10:54:16 +0530

    Img update

--- src/features/main/components/CategorySection.tsx
+++ src/features/main/components/CategorySection.tsx
@@ -1,10 +1,12 @@
 import personal from "../assets/1.jpg";
 import vehicles from "../assets/2.jpg";
+import electronics from "../assets/3.jpg";
+import living from "../assets/4.jpg";
 
 const CategorySection = () => {
   return (
     <section>
-      <h1>Category Section</h1>
+      <h1>Shop By Category</h1>
 
       <div className="category-wrap">
         <div className="category-mini-wrapper">
@@ -15,8 +17,12 @@ const CategorySection = () => {
           <div className="category-item portrait">Fashion</div>
         </div>
         <div className="category-mini-wrapper">
-          <div className="category-item portrait">Electronics</div>
-          <div className="category-item portrait">Living</div>
+          <div className="category-item portrait">
+            <img src={electronics} alt="" />
+          </div>
+          <div className="category-item portrait">
+            <img src={living} alt="" />
+          </div>
           <div className="category-item landscape">
             <img src={vehicles} alt="" />
           </div>
