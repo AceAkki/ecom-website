5148789b25e840b32b39030c2620749b7ea41615
Akshay R (A-K-SHA-Y) <akkitricks@gmail.com>
2026-03-23 23:54:13 +0530

    state based update

--- src/features/main/components/CategorySection.tsx
+++ src/features/main/components/CategorySection.tsx
@@ -1,39 +1,62 @@
+import { useState } from "react";
 import personal from "../assets/1.jpg";
 import vehicles from "../assets/2.jpg";
 import electronics from "../assets/3.jpg";
 import living from "../assets/4.jpg";
-import menFashion from "../assets/5.jpg";
-import womenFashion from "../assets/6.jpg";
+import womenFashion from "../assets/5.jpg";
+import menFashion from "../assets/6.jpg";
 
 const CategorySection = () => {
+  let [activeID, setActiveID] = useState<number | null>(null);
+  let categoryObj = [
+    {
+      name: "personal",
+      src: personal,
+    },
+    {
+      name: "electronics",
+      src: electronics,
+    },
+    {
+      name: "living",
+      src: living,
+    },
+    {
+      name: "men Fashion",
+      src: menFashion,
+    },
+    {
+      name: "women Fashion",
+      src: womenFashion,
+    },
+    {
+      name: "vehicles",
+      src: vehicles,
+    },
+  ];
+
+  let categoryElems = categoryObj.map((category, i) => {
+    let imgType = i === 0 || i === 5 ? "landscape" : "portrait";
+    return (
+      <div
+        onMouseOver={() => setActiveID(i)}
+        onMouseOut={() => setActiveID(null)}
+        className={`category-item ${imgType} `}
+      >
+        {activeID === i && (
+          <span className="category-item-overlay">{category.name}</span>
+        )}
+
+        <img src={category.src} alt="" />
+      </div>
+    );
+  });
+
   return (
     <section className="category-section">
       <h1>Shop By Category</h1>
 
-      <div className="category-wrap">
-        <div className="category-mini-wrapper">
-          <div className="category-item landscape">
-            <img src={personal} alt="" />
-          </div>
-          <div className="category-item portrait">
-            <img src={menFashion} alt="" />
-          </div>
-          <div className="category-item portrait">
-            <img src={womenFashion} alt="" />
-          </div>
-        </div>
-        <div className="category-mini-wrapper">
-          <div className="category-item portrait">
-            <img src={electronics} alt="" />
-          </div>
-          <div className="category-item portrait">
-            <img src={living} alt="" />
-          </div>
-          <div className="category-item landscape">
-            <img src={vehicles} alt="" />
-          </div>
-        </div>
-      </div>
+      <div className="category-wrap">{categoryElems}</div>
     </section>
   );
 };
