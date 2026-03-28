09c97c227adb5ec81649ecc8ffb88f2d47545b69
Akshay R (A-K-SHA-Y) <akkitricks@gmail.com>
2026-03-28 23:47:47 +0530

    not found page update

--- src/features/NotFound.tsx
+++ src/features/NotFound.tsx
@@ -1,4 +1,13 @@
+import { useEffect } from "react";
+import { useNavigate } from "react-router-dom";
+
 const NotFoundPage = () => {
+  let navigate = useNavigate();
+  useEffect(() => {
+    setTimeout(() => {
+      navigate("/");
+    }, 5000);
+  }, []);
   return (
     <>
       <h1> NotFound</h1>
