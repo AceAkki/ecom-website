6ab6623ba41b2da934cbaa0c3462816dbd2e4db2
Akshay R (A-K-SHA-Y) <akkitricks@gmail.com>
2026-03-31 23:41:16 +0530

    major ui up

--- src/features/Products/components/Product.tsx
+++ src/features/Products/components/Product.tsx
@@ -1,12 +1,19 @@
+import * as Icon from "@phosphor-icons/react";
+
 const Product = ({ data }: { data: any }) => {
   return (
-    <div className="product-wrap">
+    <div className="product-card">
       <div className="product-image">
         <img src={data?.thumbnail} alt="" />
       </div>
       <div className="product-info">
-        <h2>{data?.title}</h2>
-        <p>{data?.price}</p>
+        <h2 className="product-name">{data?.title}</h2>
+        <p className="product-price">{data?.price}</p>
+      </div>
+      <div className="btn-wrap">
+        <button className="add-cart">
+          <Icon.ShoppingCartSimpleIcon />
+        </button>
       </div>
     </div>
   );
