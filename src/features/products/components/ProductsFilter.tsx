038a8c739ecd7b5f8e00f96207a131bff0891762
Akshay R (A-K-SHA-Y) <akkitricks@gmail.com>
2026-05-15 00:26:40 +0530

    filter update

--- src/features/products/components/ProductsFilter.tsx
+++ src/features/products/components/ProductsFilter.tsx
@@ -5,6 +5,7 @@ import "react-range-slider-input/dist/style.css";
 import * as Icon from "@phosphor-icons/react";
 import "./productsFilter.css";
 import type { productType } from "../productTypes";
+import { useEffect, useRef, useState } from "react";
 
 const ProductsFilter = ({ finalData }: { finalData: productType[] }) => {
   const { filters, updateFilters } = useproductsStore(
@@ -13,6 +14,8 @@ const ProductsFilter = ({ finalData }: { finalData: productType[] }) => {
       updateFilters: state.updateFilters,
     })),
   );
+  let [isShown, setIsShown] = useState(false);
+  // let filterToggleRef = useRef(null);
   let brands = ["All", ...new Set(finalData.map((product) => product.brand))];
   let priceRange = finalData
     .map((product) => product.price)
@@ -22,79 +25,89 @@ const ProductsFilter = ({ finalData }: { finalData: productType[] }) => {
 
   return (
     <div className="product-filter-wrap">
-      <div className="filter-wrap">
-        <h5 className="filter-title"> Price Range</h5>
-        <label htmlFor="min">
-          <div className="price-input-wrap">
-            <RangeSlider
-              value={filters.priceRange}
-              min={minPrice}
-              max={maxPrice}
-              onInput={(val: [number, number]) =>
-                updateFilters({ priceRange: val })
-              }
-            />
+      <div className="filter-head-wrap">
+        <h5 className="filter-title"> Filters</h5>
+        <button className="filter-toggle" onClick={() => setIsShown(!isShown)}>
+          <Icon.FadersIcon />
+        </button>
+      </div>
+      {isShown && (
+        <>
+          <div className="filter-wrap">
+            <h5 className="filter-title"> Price Range</h5>
+            <label htmlFor="min">
+              <div className="price-input-wrap">
+                <RangeSlider
+                  value={filters.priceRange}
+                  min={minPrice}
+                  max={maxPrice}
+                  onInput={(val: [number, number]) =>
+                    updateFilters({ priceRange: val })
+                  }
+                />
+              </div>
+            </label>
           </div>
-        </label>
-      </div>
-      <hr className="filter-divider" />
-      <div className="filter-wrap">
-        <h5 className="filter-title"> Ratings</h5>
-        <div className="ratings-wrapper">
-          {Array(...Array(5))
-            .map((_, i) => i + 1)
-            .sort((a, b) => b - a)
-            .map((el) => {
-              return (
-                <button
-                  className="rating-btn"
-                  key={el}
-                  onClick={() => updateFilters({ rating: el })}
-                >
-                  <input
-                    type="checkbox"
-                    checked={filters.rating === el}
-                    readOnly
-                  />
-                  <Icon.StarIcon weight="fill" size={32} /> {el} & Up
-                </button>
-              );
-            })}
-        </div>
-      </div>
-      <hr className="filter-divider" />
-      <div className="filter-wrap">
-        <h5 className="filter-title"> Availability</h5>
-        <label htmlFor="status" className="status-wrap">
-          <input
-            type="checkbox"
-            name="status"
-            onChange={() =>
-              updateFilters({ inStockOnly: !filters.inStockOnly })
-            }
-          />
-          <p>Stock Availability Status</p>
-        </label>
-      </div>
-      <hr className="filter-divider" />
-      <div className="filter-wrap">
-        <h5 className="filter-title"> Brands</h5>
-        <select
-          name="brands"
-          value={filters.brand}
-          onChange={(e) => {
-            updateFilters({ brand: e.target.value });
-          }}
-        >
-          {brands.map((brand) => {
-            return (
-              <option key={brand} value={brand}>
-                {brand}
-              </option>
-            );
-          })}
-        </select>
-      </div>
+          <hr className="filter-divider" />
+          <div className="filter-wrap">
+            <h5 className="filter-title"> Ratings</h5>
+            <div className="ratings-wrapper">
+              {Array(...Array(5))
+                .map((_, i) => i + 1)
+                .sort((a, b) => b - a)
+                .map((el) => {
+                  return (
+                    <button
+                      className="rating-btn"
+                      key={el}
+                      onClick={() => updateFilters({ rating: el })}
+                    >
+                      <input
+                        type="checkbox"
+                        checked={filters.rating === el}
+                        readOnly
+                      />
+                      <Icon.StarIcon weight="fill" size={32} /> {el} & Up
+                    </button>
+                  );
+                })}
+            </div>
+          </div>
+          <hr className="filter-divider" />
+          <div className="filter-wrap">
+            <h5 className="filter-title"> Availability</h5>
+            <label htmlFor="status" className="status-wrap">
+              <input
+                type="checkbox"
+                name="status"
+                onChange={() =>
+                  updateFilters({ inStockOnly: !filters.inStockOnly })
+                }
+              />
+              <p>Stock Availability Status</p>
+            </label>
+          </div>
+          <hr className="filter-divider" />
+          <div className="filter-wrap">
+            <h5 className="filter-title"> Brands</h5>
+            <select
+              name="brands"
+              value={filters.brand}
+              onChange={(e) => {
+                updateFilters({ brand: e.target.value });
+              }}
+            >
+              {brands.map((brand) => {
+                return (
+                  <option key={brand} value={brand}>
+                    {brand}
+                  </option>
+                );
+              })}
+            </select>
+          </div>
+        </>
+      )}
     </div>
   );
 };
