98ceabb6667686af1152fca0bad85afd29e5b495
Akshay R (A-K-SHA-Y) <akkitricks@gmail.com>
2026-06-03 23:55:48 +0530

    motion without anime

--- src/features/main/components/MainBanner.tsx
+++ src/features/main/components/MainBanner.tsx
@@ -31,10 +31,10 @@ const MainBanner = () => {
         </div>
         <motion.div
           className="home-page-btn"
-          initial={{ opacity: 0, scale: 0, y: 50 }}
-          whileInView={{ opacity: 1, scale: 1, y: 0 }}
-          transition={{ duration: 0.8, ease: "easeOut" }}
-          viewport={{ amount: 1 }}
+          // initial={{ opacity: 0, scale: 0, y: 50 }}
+          // whileInView={{ opacity: 1, scale: 1, y: 0 }}
+          // transition={{ duration: 0.8, ease: "easeOut" }}
+          // viewport={{ amount: 1 }}
         >
           <Link to="/products">Shop Now</Link>
         </motion.div>
