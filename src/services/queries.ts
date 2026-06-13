3442fd40ef1831621e2e286cdb39b288e06b85f8
Akshay R (A-K-SHA-Y) <akkitricks@gmail.com>
2026-06-13 23:15:09 +0530

    update

--- src/services/queries.ts
+++ src/services/queries.ts
@@ -7,27 +7,21 @@ export const productQueries = {
     queryOptions({
       queryKey: [defaultParam] as string[],
       queryFn: () => fetchProductsData({}),
-      // During this time, no new network requests will be made
       staleTime: Infinity,
-      // How long the data stays in memory after the component unmounts
       gcTime: 10,
     }),
   idData: (id: string) =>
     queryOptions({
       queryKey: [id],
       queryFn: () => fetchProductsData({ id: id }),
-      // During this time, no new network requests will be made
       staleTime: Infinity,
-      // How long the data stays in memory after the component unmounts
       gcTime: 10,
     }),
   categoryData: (category: string) =>
     queryOptions({
       queryKey: [defaultParam, category],
       queryFn: () => fetchProductsData({ category: category }),
-      // During this time, no new network requests will be made
       staleTime: Infinity,
-      // How long the data stays in memory after the component unmounts
       gcTime: 10 * 60 * 1000,
     }),
 };
