0fa4fb6cb053476821c7177f18962381759512e1
Akshay R (A-K-SHA-Y) <akkitricks@gmail.com>
2026-06-21 21:06:07 +0530

    update

--- src/features/HelpPage.tsx
+++ src/features/HelpPage.tsx
@@ -2,6 +2,12 @@ const HelpPage = () => {
   return (
     <>
       <h1> Help Page</h1>
+      <p>
+        Lorem ipsum dolor sit amet consectetur adipisicing elit. Dolorem aliquid
+        repellat enim. Dignissimos aspernatur quos quidem accusamus temporibus
+        illo autem, dolore praesentium harum sunt consequatur repellat nam
+        itaque, sequi maiores.
+      </p>
     </>
   );
 };
