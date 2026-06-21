d810cd610960329cff05ef8980a910bee0ad95ca
Akshay R (A-K-SHA-Y) <akkitricks@gmail.com>
2026-06-21 21:13:55 +0530

    update para

--- src/features/AboutPage.tsx
+++ src/features/AboutPage.tsx
@@ -2,6 +2,12 @@ const AboutPage = () => {
   return (
     <>
       <h1> About Page </h1>
+      <p>
+        Lorem ipsum dolor sit amet consectetur adipisicing elit. Quaerat placeat
+        sed fugit, minus aspernatur aliquam numquam recusandae culpa vitae?
+        Praesentium debitis ipsum iste cupiditate dicta libero asperiores ex
+        blanditiis? Est.
+      </p>
     </>
   );
 };
