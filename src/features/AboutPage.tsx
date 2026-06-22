d61f8597410befd9f62a4b1fd2be56350d9836c5
Akshay R (A-K-SHA-Y) <akkitricks@gmail.com>
2026-06-22 23:11:50 +0530

    up

--- src/features/AboutPage.tsx
+++ src/features/AboutPage.tsx
@@ -2,18 +2,6 @@ const AboutPage = () => {
   return (
     <>
       <h1> About Page </h1>
-      <p>
-        Lorem ipsum dolor sit amet consectetur adipisicing elit. Quaerat placeat
-        sed fugit, minus aspernatur aliquam numquam recusandae culpa vitae?
-        Praesentium debitis ipsum iste cupiditate dicta libero asperiores ex
-        blanditiis? Est.
-      </p>
-      <p>
-        Lorem ipsum dolor sit amet consectetur, adipisicing elit. Exercitationem
-        esse minima aliquid neque, dignissimos voluptate iusto optio, in a
-        architecto libero amet sequi officia recusandae dolor inventore quo.
-        Placeat, quis?
-      </p>
     </>
   );
 };
