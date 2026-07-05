a92ba4113a8fadc443f46938ffc68c96c4c757fe
Akshay R (A-K-SHA-Y) <akkitricks@gmail.com>
2026-07-05 23:37:14 +0530

    update

--- src/features/ErrorPage.tsx
+++ src/features/ErrorPage.tsx
@@ -6,6 +6,12 @@ const ErrorPage = () => {
       <h1> An Error Occured !!</h1>
       <hr />
       <p>{error?.message}</p>
+      <p>
+        Lorem ipsum dolor, sit amet consectetur adipisicing elit. Voluptatem,
+        incidunt esse animi quisquam voluptatibus labore quo exercitationem sit
+        minima tempora neque earum vel quasi itaque voluptate illum! Veniam,
+        amet vel?
+      </p>
     </>
   );
 };
--- src/features/HelpPage.tsx
+++ src/features/HelpPage.tsx
@@ -9,6 +9,18 @@ const HelpPage = () => {
         illo autem, dolore praesentium harum sunt consequatur repellat nam
         itaque, sequi maiores.
       </p>
+      <p>
+        Lorem ipsum, dolor sit amet consectetur adipisicing elit. In
+        exercitationem doloribus aliquam libero asperiores a dignissimos,
+        voluptate perspiciatis nam omnis. Accusantium hic molestiae odio,
+        mollitia omnis quod at velit similique.
+      </p>
+      <p>
+        Lorem ipsum dolor sit amet consectetur adipisicing elit. Culpa possimus
+        atque eaque laboriosam, nobis nostrum nam, quidem impedit voluptate quia
+        illo beatae? Quasi doloremque, cupiditate commodi officia aspernatur
+        nobis quibusdam.
+      </p>
     </>
   );
 };
