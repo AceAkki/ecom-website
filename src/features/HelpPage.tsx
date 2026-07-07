ff46a6750c1872ea343aa4ac7e2c63ea7e880bf9
Akshay R (A-K-SHA-Y) <akkitricks@gmail.com>
2026-07-07 23:01:13 +0530

    para update

--- src/features/HelpPage.tsx
+++ src/features/HelpPage.tsx
@@ -3,23 +3,6 @@ const HelpPage = () => {
     <>
       <h1> Help Page</h1>
       <hr />
-      <p>
-        Lorem ipsum dolor sit amet, consectetur adipisicing elit. Consectetur
-        fugiat quibusdam vel, deleniti alias sunt maiores itaque enim error eos,
-        blanditiis earum ullam labore. Esse accusamus dolorem consectetur nemo
-        repellendus.
-      </p>
-      <p>
-        Lorem ipsum dolor sit amet, consectetur adipisicing elit. Suscipit, et
-        natus eligendi quos voluptate numquam at ex quas vel corrupti aspernatur
-        ab quo rem tenetur iste enim omnis aperiam! Autem!
-      </p>
-      <p>
-        Lorem ipsum dolor sit amet, consectetur adipisicing elit. Quibusdam
-        nulla, maiores sed doloremque eum voluptatem ut fugit eius praesentium
-        consectetur totam laudantium odit fugiat consequatur neque velit
-        voluptas molestias cupiditate?
-      </p>
     </>
   );
 };
