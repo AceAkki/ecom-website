efea68139897b106780e48d9b5680c98a86212ca
Akshay R (A-K-SHA-Y) <akkitricks@gmail.com>
2026-07-07 23:00:37 +0530

    para up

--- src/features/AboutPage.tsx
+++ src/features/AboutPage.tsx
@@ -3,7 +3,24 @@ const AboutPage = () => {
     <>
       <h1> About Page </h1>
       <hr />
-      
+      <p>
+        Lorem, ipsum dolor sit amet consectetur adipisicing elit. Eius
+        voluptates quaerat beatae est quae debitis architecto officia nulla
+        possimus et sit animi ipsam totam facere modi voluptatum aliquid,
+        incidunt excepturi?
+      </p>
+      <p>
+        Lorem ipsum dolor sit, amet consectetur adipisicing elit. Sint
+        laboriosam nostrum, vitae numquam, quod fugit corrupti exercitationem
+        quae nulla nihil quos inventore provident animi labore accusantium ut.
+        Aperiam, voluptate harum.
+      </p>
+      <p>
+        Lorem ipsum, dolor sit amet consectetur adipisicing elit. Rerum, ex quam
+        voluptates voluptatem illo reprehenderit. Tempore placeat minus deleniti
+        fuga, doloribus amet laborum totam itaque modi veniam nemo praesentium
+        perspiciatis.
+      </p>
     </>
   );
 };
