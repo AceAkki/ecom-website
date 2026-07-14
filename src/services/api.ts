9b2fc96bab3fdb703b1349ed896a22bddcdcc811
Akshay R (A-K-SHA-Y) <akkitricks@gmail.com>
2026-07-15 00:01:19 +0530

    comment update

--- src/services/api.ts
+++ src/services/api.ts
@@ -3,12 +3,11 @@ export type ExchangeRates = Record<string, number>;
 
 export const fetchCurrencyData = async () => {
   try {
+    // fetches currency url
     const res = await fetch("https://open.er-api.com/v6/latest/USD");
     const data = await res.json();
-    // console.log(data);
-    if (data) {
-      return data;
-    }
+    // returns if data exits
+    if (data) return data;
   } catch (error) {
     console.log(error);
   }
