1278b6dadadea3d8703640a7364b1d9e4c7c5573
Akshay R (A-K-SHA-Y) <akkitricks@gmail.com>
2026-07-15 20:52:46 +0530

    update component

--- src/components/ui/FallbackLoader.tsx
+++ src/components/ui/FallbackLoader.tsx
@@ -1,14 +1,14 @@
 import { ThreeCircles } from "react-loader-spinner";
 
 // three circles loader - displays when page is loading
-export default function FallBackLoader({color:"#2563eb"}) {
+export default function FallBackLoader() {
   return (
     <section className="loader-wrapper">
       <ThreeCircles
         visible={true}
         height="100"
         width="100"
-        color={color}
+        color="#2563eb"
         ariaLabel="three-circles-loading"
         wrapperStyle={{}}
         wrapperClass=""
