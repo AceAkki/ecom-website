881b14124458f0949e11001b817944d363466dc7
Akshay R (A-K-SHA-Y) <akkitricks@gmail.com>
2026-07-16 23:17:23 +0530

    firebase comment

--- src/services/firebase.ts
+++ src/services/firebase.ts
@@ -46,7 +46,7 @@ export let mainCategories = {
   ],
 };
 
-// need to shorten this long function- its handling too much right now
+// need to shorten this long function- its handling too much right now, it is bloated right now
 export async function fetchProductsData({
   category,
   id,
