c6856304106d4f0b86689b8d830cc0ec0c943924
Akshay R (A-K-SHA-Y) <akkitricks@gmail.com>
2026-07-17 20:58:00 +0530

    removed unused

--- src/features/AboutPage.tsx
+++ src/features/AboutPage.tsx
@@ -3,18 +3,6 @@ const AboutPage = () => {
     <>
       <h1> About Page </h1>
       <hr />
-      <p>
-        Lorem, ipsum dolor sit amet consectetur adipisicing elit. Eius
-        voluptates quaerat beatae est quae debitis architecto officia nulla
-        possimus et sit animi ipsam totam facere modi voluptatum aliquid,
-        incidunt excepturi?
-      </p>
-      <p>
-        Lorem ipsum dolor sit, amet consectetur adipisicing elit. Sint
-        laboriosam nostrum, vitae numquam, quod fugit corrupti exercitationem
-        quae nulla nihil quos inventore provident animi labore accusantium ut.
-        Aperiam, voluptate harum.
-      </p>
     </>
   );
 };
