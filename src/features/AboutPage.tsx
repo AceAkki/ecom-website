c32502d17a50881d3c07574b66093e827e29f9eb
Akshay R (A-K-SHA-Y) <akkitricks@gmail.com>
2026-07-21 22:22:13 +0530

    css update

--- src/features/AboutPage.tsx
+++ src/features/AboutPage.tsx
@@ -3,12 +3,6 @@ const AboutPage = () => {
     <>
       <h1> About Page </h1>
       <hr />
-      <p>
-        Lorem ipsum, dolor sit amet consectetur adipisicing elit. Beatae
-        explicabo incidunt earum dolorem molestias! Accusantium ex minima veniam
-        nulla omnis aut recusandae repudiandae dolor voluptatibus, earum nisi,
-        itaque quasi. Perspiciatis!
-      </p>
     </>
   );
 };
